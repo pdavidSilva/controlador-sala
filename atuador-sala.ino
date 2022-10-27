7074ae5deb3b3c86695540b0691f6659b1ee457d
abraaoalves <abraao1514@gmail.com>
2022-10-27 01:19:15 -0300

    refactor: commit auxiliar ao anterior

--- atuador-sala.ino
+++ atuador-sala.ino
@@ -5,8 +5,8 @@ HardwareRecord hardware;
 Controller controller;
 String master = "";
 
-bool HAS_IR_TO_SEND = false;
-String COMMAND_IR;
+bool SEND_DATA = false;
+String COMMAND;
 const uint16_t kIrLed = 12;
 IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
 
@@ -35,12 +35,12 @@ void setup() {
 
 void loop() {
   Serial.println("[Loop] Await message"); 
-  if(HAS_IR_TO_SEND) {
+  if(SEND_DATA) {
     Serial.println("[Loop] FOWARD TO SEND IR"); 
-    HAS_IR_TO_SEND = false;
-    controller.ExecuteCommandIR(COMMAND_IR);
+    SEND_DATA = false;
+    controller.ExecuteCommandIR(COMMAND);
   }
 
-  delay(2000);
+  delay(500);
 }
 */
